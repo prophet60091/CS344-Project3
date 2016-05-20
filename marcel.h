5dee01c8cff8f0f0607d5b38455475bcccb63a6e
Robert Jackson <jackrobe@onid.oregonestate.edu>
2016-05-20 15:45:22 -0600

    add type to exec_cmd

--- marcel.h
+++ marcel.h
@@ -6,7 +6,7 @@
 #define PROJECT3_MARCEL_H
 
 int exec_inShell(char * c, char * a);
-exec_cmd(char *c, char *a);
+void exec_cmd(char *c, char *a);
 void get_cmd(char **c, char **a);
 int error(char *msg);
 size_t array_byteCount(char * arr);
