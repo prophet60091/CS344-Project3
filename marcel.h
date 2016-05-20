bec00a23bcbe82a28acc746ac549bbd9bf5d5f9e
Robert Jackson <jackrobe@onid.oregonestate.edu>
2016-05-20 15:32:09 -0600

    added a header file

---
+++ marcel.h
@@ -0,0 +1,14 @@
+//
+// Created by Robert on 5/20/2016.
+//
+
+#ifndef PROJECT3_MARCEL_H
+#define PROJECT3_MARCEL_H
+
+int exec_inShell(char * c, char * a);
+exec_cmd(char *c, char *a);
+void get_cmd(char **c, char **a);
+int error(char *msg);
+size_t array_byteCount(char * arr);
+
+#endif //PROJECT3_MARCEL_H
