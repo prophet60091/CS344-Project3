d79ad5e33f4fc3a2787fde9c7aa133066f8e9030
Robert Jackson <jackrobe@onid.oregonestate.edu>
2016-05-23 10:06:39 -0600

    add functions to begin collecting pids added beginngin check for background ***Upload is for testing on EOS**

--- marcel.c
+++ marcel.c
@@ -27,13 +27,77 @@ const int ARGSIZE = 512; // max size of args
 const int MAXARGS = 2560;
 char *cmd;
 int status;
+int BIStatus = 0;
 
+typedef struct kiddos{
+    pid_t * childProcs;
+    int size;
+    int cap;
+}kiddos;
 
 //error function
 int error(char *msg)
 {
     perror(msg);
     return 1;
+}
+
+
+//Struct Array functions - adapted from CS261, 2014
+void _initKiddos(kiddos *v, int capacity)
+{
+    assert(capacity > 0);
+    assert(v!= 0);
+    v->childProcs = malloc(sizeof(pid_t) * capacity);
+    assert(v->childProcs != 0);
+    v->size = 0;
+    v->cap = capacity;
+
+}
+
+kiddos* createKiddos(int cap)
+{
+    kiddos *r;
+    assert(cap > 0);
+    r = malloc(sizeof( kiddos));
+    assert(r != 0);
+    _initKiddos(r,cap);
+    return r;
+}
+
+/* Deallocate data array in Kiddos.
+
+	param: 	v		pointer to the dynamic array
+	pre:    v is not null
+	post:	d.data points to null
+	post:	size and capacity are 0
+	post:	the memory used by v->data is freed
+*/
+void freeKiddos(kiddos *v)
+{
+    assert(v!=0);
+
+    if(v->childProcs != 0)
+    {
+        free(v->childProcs); 	/* free the space on the heap */
+        v->childProcs = 0;   	/* make it point to null */
+    }
+    v->size = 0;
+    v->cap = 0;
+}
+
+/* Deallocate data array and the dynamic array ure.
+
+	param: 	v		pointer to the dynamic array
+	pre:	v is not null
+	post:	the memory used by v->data is freed
+	post:	the memory used by d is freed
+*/
+void deleteKiddos(kiddos *v)
+{
+    assert (v!= 0);
+    freeKiddos(v);
+    free(v);
 }
 
 int open_file(char * fileName, int rw){
@@ -49,12 +113,19 @@ int open_file(char * fileName, int rw){
     if(fd < 0) { // opening fails
 
         error("Couldn't open file " );
-        return -1;
+        BIStatus =1;
+        return 1;
     }
 
     return fd; // returning the file pointer
 }
 
+size_t addChild(kiddos* kids, pid_t id){
+
+    kids->childProcs[kids->size]= id;
+    kids->size++;
+
+}
 
 //#################### Get Direction of the user
 // sets the ans variable for use in other functions
@@ -98,6 +169,9 @@ char** get_cmd(){
 
 }
 
+
+//TODO Alter this so that it returns-maybe the number of redirs?
+//make a separate to determine positions
 int checkRedirect(char ** cmd){
     int pos = 0;
 
@@ -111,6 +185,22 @@ int checkRedirect(char ** cmd){
     return 0;
 
 }
+
+
+int checkBackground(char ** cmd){
+    int pos = 0;
+
+    while(cmd[pos] != NULL){
+
+        if((strcmp(cmd[pos], "&") == 0)){
+            return pos;
+        }
+        pos++;
+    }
+    return 0;
+
+}
+
 
 int checkCmdSize(char ** cmd){
     int pos = 0;
@@ -171,7 +261,8 @@ int exec_cmd(char **cmd){
 
         if(checkCmdSize(cmd) <= 1){
             cmd = realloc(cmd, sizeof(char*)*2);
-            cmd[1]= "~";
+            cmd[1]= getenv("HOME");
+
         }
         //change the directory, and if failed putout stderror
         if(chdir(cmd[1]) !=0){
@@ -184,7 +275,12 @@ int exec_cmd(char **cmd){
             printf("%i", status);
             return 0;
 
-    }else{
+    //we have a comment ignore it
+    }else if((strncmp(cmd[0] , "#", 1) == 0)){
+
+        return 0;
+
+    } else{
 
         // not a built-in? Execute it.
         return exec_inShell(cmd);
@@ -209,21 +305,35 @@ int exec_inShell(char ** cmd){
 
         case -1:
             //it's in a bad state
-            status = error("boom!");
+            status = 1;
+            error("boom!");
             return status;
 
         case 0:
 
+
             //if process has a redirect:
             if ((rpos = checkRedirect(cmd)) > 0){
 
-                fd = changeOut(cmd, rpos);
+                fd = changeOut(cmd, rpos);  //alter the redirects as needed
 
                 //strip out the end of cmd we wont need it anymore
                 cmd[rpos] = NULL;
                 cmd = realloc(cmd, (size_t)rpos);
 
+                //REQ print out if file cannot be created
+                if(fd > 0){
+
+                    error("Could not redirect");
+                    exit(1);
+                }
             }
+
+            //If process has a &
+            if(rpos = checkBackground(cmd)){
+
+            }
+
 
            //child process execute
             status = execvp(cmd[0], cmd);
@@ -232,24 +342,29 @@ int exec_inShell(char ** cmd){
             if(status < 0){
 
                 //kill it!!
+                fprintf(stdout, "Child process quit with status: %i", status);
+                //unless exec fails  this never executes
+                error("bam!");
+                BIStatus = 1;
+                return status;
 
             }else{ //child did something  well, praise it
 
-                //there was a redirect we need to close a file
+                //there was a redirect and it finished;, we need to close a file
                 if(fd > 0){
-                  test=  close(fd);
+                    test=  close(fd);
                     printf("%i", test);
                 }
 
+                return status;
             }
 
 
-            //unless exec fails  this never executes
-            error("bam!");
-            return status;
+
 
         default:
             do {
+
                 wpid = waitpid(pcessID, &status, WUNTRACED);
 
             } while (!WIFEXITED(status) && !WIFSIGNALED(status) );
@@ -262,21 +377,39 @@ int exec_inShell(char ** cmd){
             // we fisnishd a process and we have a redirect - better close the file;
             // it might need to also be reset.
 
-            printf("Child process id that completed is %i\n", (int)wpid);
-            printf("Child process exit status %i\n", status);
+            fprintf(stdout, "Child process id that completed is %i\n", (int)wpid);
+            fprintf(stdout, "Child process exit status %i\n", status);
             return status;
     }
 }
 
 int main(int argc, char *argv[]){
+    //Initialzations:
+    kiddos* kids;
+    kids = createKiddos(100);
 
     while(status == 0){
 
         char ** cmd = NULL;
         cmd = get_cmd(); // get the command from user
-        status = exec_cmd(cmd); // exec on it
+
+        if(cmd[0] != NULL){ // it's not blank
+            status = exec_cmd(cmd); // exec on it
+        }
+
 
        // printf("Redirect is %i, cmdSize is %i", checkRedirect(cmd), checkCmdSize(cmd) );
     }
+    free(cmd);
+
+
+//    //we'll use this and others to store the kids, and check them delete them etc.
+//     addChild(kids, getpid()) ;
+//    int i = 0;
+//    for(i=0; i < kids->size; i++ ){
+//        printf("%i", (int)kids->childProcs[i]);
+//    }
+
+
 
 }
